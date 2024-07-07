c51aaafda2fb30dd2b6a0bb9b3d52a6f2de51e4d
René Löscher <rloescher@newmoonproduction.com>
2024-07-07 14:12:24 +0200

    setup project; added input entity

---
+++ Assets/Scripts/GameControls/Components/CurrentGameInput.cs
@@ -0,0 +1,8 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+namespace PotatoFinch.TmgDotsJam.GameControls {
+	public struct CurrentGameInput : IComponentData {
+		public Vector2 CurrentMovementInputVector;
+	}
+}
---
+++ Assets/Scripts/GameControls/Systems/HandleGameInputSystem.cs
@@ -0,0 +1,36 @@
+﻿using Unity.Entities;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace PotatoFinch.TmgDotsJam.GameControls {
+	public partial class HandleGameInputSystem : SystemBase {
+		private GameInputActions _gameInputActions;
+
+		private Vector2 _currentMovementInputVector;
+
+		protected override void OnCreate() {
+			_gameInputActions = new GameInputActions();
+			_gameInputActions.Enable();
+			
+			_gameInputActions.Gameplay.PlayerMovement.performed += OnPlayerMovementPerformed;
+			_gameInputActions.Gameplay.PlayerMovement.canceled += OnPlayerMovementCanceled;
+
+			EntityManager.CreateSingleton<CurrentGameInput>();
+		}
+
+		private void OnPlayerMovementCanceled(InputAction.CallbackContext callbackContext) {
+			_currentMovementInputVector = Vector2.zero;
+		}
+
+		private void OnPlayerMovementPerformed(InputAction.CallbackContext callbackContext) {
+			var currentMovementInputVector = callbackContext.ReadValue<Vector2>();
+			_currentMovementInputVector = currentMovementInputVector;
+		}
+
+		protected override void OnUpdate() {
+			SystemAPI.SetSingleton(new CurrentGameInput {
+				CurrentMovementInputVector = _currentMovementInputVector,
+			});
+		}
+	}
+}
