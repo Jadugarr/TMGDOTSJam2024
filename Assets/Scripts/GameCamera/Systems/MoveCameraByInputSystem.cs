3aaa50a7f09f674bec4695ea69dfe1c242fc3666
René Löscher <rloescher@newmoonproduction.com>
2024-07-08 18:32:23 +0200

    moving camera follow target according to camera rotation

--- Assets/Scripts/GameCamera/Systems/MoveCameraByInputSystem.cs
+++ Assets/Scripts/GameCamera/Systems/MoveCameraByInputSystem.cs
@@ -12,21 +12,25 @@ namespace PotatoFinch.TmgDotsJam.GameCamera {
 		public void OnCreate(ref SystemState state) {
 			state.RequireForUpdate<CameraFollowTargetTag>();
 			state.RequireForUpdate<CurrentGameInput>();
+			state.RequireForUpdate<GameCameraComponent>();
 		}
 
-		[BurstCompile]
 		public void OnUpdate(ref SystemState state) {
 			var currentInput = SystemAPI.GetSingleton<CurrentGameInput>();
 
 			if (math.all(currentInput.CurrentMovementInputVector == float2.zero)) {
 				return;
 			}
-			
+
+			var cameraComponent = SystemAPI.ManagedAPI.GetSingleton<GameCameraComponent>();
 			var cameraFollowTargetEntity = SystemAPI.GetSingletonEntity<CameraFollowTargetTag>();
 			var movementSpeed = SystemAPI.GetComponentRO<MovementSpeed>(cameraFollowTargetEntity);
 			var localTransform = SystemAPI.GetComponentRW<LocalTransform>(cameraFollowTargetEntity);
 
-			localTransform.ValueRW.Position += new float3(currentInput.CurrentMovementInputVector.x, 0f, currentInput.CurrentMovementInputVector.y) * movementSpeed.ValueRO.Value * SystemAPI.Time.DeltaTime;
+			var movementVector = new float3(currentInput.CurrentMovementInputVector.x, 0f, currentInput.CurrentMovementInputVector.y);
+			movementVector = cameraComponent.Value.transform.rotation * movementVector;
+			movementVector.y = 0f;
+			localTransform.ValueRW.Position += movementVector * movementSpeed.ValueRO.Value * SystemAPI.Time.DeltaTime;
 		}
 
 		[BurstCompile]
