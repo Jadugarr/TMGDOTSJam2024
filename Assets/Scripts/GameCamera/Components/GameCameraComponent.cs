1c414503257d945cb081c20b7f4a2e0fed9967c7
René Löscher <rloescher@newmoonproduction.com>
2024-07-08 18:27:26 +0200

    created camera singleton entity

---
+++ Assets/Scripts/GameCamera/Components/GameCameraComponent.cs
@@ -0,0 +1,8 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+namespace PotatoFinch.TmgDotsJam.GameCamera {
+	public class GameCameraComponent : IComponentData {
+		public Camera Value;
+	}
+}
---
+++ Assets/Scripts/GameCamera/Systems/InitializeCameraEntitySystem.cs
@@ -0,0 +1,27 @@
+﻿using Unity.Burst;
+using Unity.Entities;
+using UnityEngine;
+
+namespace PotatoFinch.TmgDotsJam.GameCamera {
+	[UpdateInGroup(typeof(GameCameraSystemGroup), OrderFirst = true)]
+	public partial struct InitializeCameraEntitySystem : ISystem, ISystemStartStop {
+		public void OnCreate(ref SystemState state) {
+		}
+
+		public void OnStartRunning(ref SystemState state) {
+			var gameCameraEntity = state.EntityManager.CreateEntity(typeof(GameCameraComponent));
+			state.EntityManager.SetComponentData(gameCameraEntity, new GameCameraComponent { Value = Camera.main });
+		}
+
+		public void OnStopRunning(ref SystemState state) {
+		}
+
+		[BurstCompile]
+		public void OnUpdate(ref SystemState state) {
+		}
+
+		[BurstCompile]
+		public void OnDestroy(ref SystemState state) {
+		}
+	}
+}
