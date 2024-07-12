195f2dd2a2c993a732a5b95bd830fc5126d2fbf0
René Löscher <rloescher@newmoonproduction.com>
2024-07-12 20:33:16 +0200

    show remaining loop time in UI

---
+++ Assets/Scripts/UI/RemainingGameTimeDisplay.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using PotatoFinch.TmgDotsJam.GameTime;
+using TMPro;
+using Unity.Entities;
+using UnityEngine;
+
+namespace PotatoFinch.TmgDotsJam.UI {
+	public class RemainingGameTimeDisplay : MonoBehaviour {
+		[SerializeField] private TMP_Text _textfield;
+
+		private EntityQuery _gameTimeQuery;
+
+		private void Awake() {
+			_gameTimeQuery = World.DefaultGameObjectInjectionWorld.EntityManager.CreateEntityQuery(typeof(GameTimeComponent));
+		}
+
+		private void Update() {
+			if (!_gameTimeQuery.TryGetSingleton(out GameTimeComponent gameTimeComponent)) {
+				return;
+			}
+
+			_textfield.text = gameTimeComponent.RemainingTime.ToString("F2", CultureInfo.InvariantCulture);
+		}
+	}
+}
